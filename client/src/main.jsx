0de5d916fdb7250179e902a60f133c404acb4721
Petko Todorov <p.todorov1015@gmail.com>
2024-07-21 13:39:23 +0300

    add BrowserRouter and Provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { UsernameProvider } from './contexts/UsernameContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <UsernameProvider>
+                <App />
+            </UsernameProvider>
+        </BrowserRouter>
     </React.StrictMode>,
 )
