564ca05e81413c3ff9704ccf7678ba612fe12d7d
Petko Todorov <p.todorov1015@gmail.com>
2024-07-25 11:53:35 +0300

    send messages works

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -4,22 +4,19 @@ import { useParams } from 'react-router-dom';
 import { UsernameContext } from '../../contexts/UsernameContext.jsx';
 import { useNavigate } from 'react-router-dom';
 // import { ConnectedUsersContext } from '../contexts/ConnectedUsersContext.jsx';
-import Notification from '../Notifications/Notification.jsx';
+import Notification from '../Notification.jsx';
 import UsersInRoom from './UsersInRoom.jsx';
-import Messages from './Messages.jsx';
 
 const socket = io('http://localhost:3000');
 
 function Chat() {
     const { roomId } = useParams();
-    // const [username, setUsername] = useState('');
-    // const [messages, setMessages] = useState([]);
-    // const [currentMessage, setCurrentMessage] = useState('');
-    // const [isConnected, setIsConnected] = useState(false);
+    const [messages, setMessages] = useState([]);
     const { username } = useContext(UsernameContext);
     const [usersInRoom, setUsersInRoom] = useState([]);
     const [notification, setNotification] = useState();
     const navigate = useNavigate();
+    const messagesEndRef = useRef(null);
 
     useEffect(() => {
         socket.emit('joinRoom', username, roomId);
@@ -33,20 +30,20 @@ function Chat() {
         const handleUserLeftChat = (userLeftChat) => {
             if (userLeftChat !== username) {
                 setNotification(`${userLeftChat} left chat`);
-            }
+            };
         };
 
         const handleUserJoinedRoom = (userJoinRoom) => {
             if (userJoinRoom.username !== username) {
                 setNotification(`${userJoinRoom.username} joined the room`);
-            }
+            };
         };
 
         const handleUserLeftRoom = (userLeftRoom) => {
             setUsersInRoom(prevUsers => prevUsers.filter(user => user !== userLeftRoom));
             if (userLeftRoom !== username) {
                 setNotification(`${userLeftRoom} left the room`);
-            }
+            };
         };
 
         socket.on('usersConnected', handleUserJoinedChat);
@@ -58,11 +55,12 @@ function Chat() {
             setUsersInRoom(users);
         });
 
-        // TODO: do 
-        socket.on('newMessage', (message) => {
-            if (message.room === roomId) {
-                setMessages(prevMessages => [...prevMessages, message]);
-            }
+        socket.on('messageHistory', (history) => {
+            setMessages(history);
+        });
+
+        socket.on('newMessage', (newMessage) => {
+            setMessages(prevMessages => [...prevMessages, newMessage]);
         });
 
         return () => {
@@ -75,6 +73,19 @@ function Chat() {
             socket.off('newMessage');
         };
     }, [roomId, username]);
+
+    useEffect(() => {
+        if (messagesEndRef.current) {
+            messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+        }
+    }, [messages]);
+
+    const handleSendMessage = (event) => {
+        event.preventDefault();
+        const message = event.target.message.value;
+        socket.emit('sendMessage', message, username, roomId);
+        event.target.message.value = '';
+    };
 
     const handleDisconnect = () => {
         socket.emit('leaveRoom', username, roomId);
@@ -91,14 +102,44 @@ function Chat() {
                 <div className='flex h-[85vh]'>
                     <UsersInRoom users={usersInRoom} />
 
-                    <div className='bg-slate-300 w-[70%] flex justify-between'>
-                        <Messages />
-                        <button
-                            className='bg-red-500 p-1 w-8 h-7'
-                            onClick={handleDisconnect}
-                        >
-                            X
-                        </button>
+                    <div className='bg-slate-300 w-[70%] flex flex-col'>
+                        <div className='flex justify-between'>
+                            room name
+                            <button
+                                className='bg-red-500 p-1 w-8 h-7'
+                                onClick={handleDisconnect}
+                            >
+                                X
+                            </button>
+                        </div>
+
+                        <hr className='w-full my-4 border-b'/>
+
+                        <div className='flex flex-col-reverse flex-grow overflow-y-auto p-2'>
+                            <div className='flex flex-col'>
+                                {messages.map((message, index) => (
+                                    <div key={index}>
+                                        {message.username} - {message.message}
+                                    </div>
+                                ))}
+                                <div ref={messagesEndRef} />
+                            </div>
+                        </div>
+
+                        <form onSubmit={handleSendMessage}
+                            className='flex items-center p-2 border-t border-gray-400'>
+                            <input
+                                name="message"
+                                id="message"
+                                type="text"
+                                className='p-2 flex-grow border border-gray-300 rounded mr-2'
+                                placeholder="Type your message..."
+                            />
+                            <button type='submit' className='bg-blue-500 text-white p-2 rounded'>
+                                Send
+                            </button>
+                        </form>
+
                     </div>
                 </div>
             </div>
