29f347928c116a8056554e05db3cd2fa9b54880a
Petko Todorov <p.todorov1015@gmail.com>
2024-07-25 15:09:25 +0300

    css styles change

--- client/src/components/Chat/UsersInRoom.jsx
+++ client/src/components/Chat/UsersInRoom.jsx
@@ -1,13 +1,25 @@
+import chatAppImage from '../../assets/chat-app2.jpg';
+
 const UsersInRoom = ({
     users
 }) => {
 
     return (
-        <div className='bg-blue-500 w-[30%]'>
-            users
+        <div className='bg-blue-500 w-[20%]'>
+            <img src={chatAppImage} height={'200px'} alt="chat-app"
+                className='w-[3.7rem] mx-auto' />
+
+            <hr className='w-full border-b' />
+
+            <h1 className='text-center text-xl p-2'>
+                Connected Users
+            </h1>
             <ul>
                 {users.map((user, index) => (
-                    <li key={index}>{user}</li>
+                    <div key={index} className='flex items-center ml-5'>
+                        <i className="fa-solid fa-circle text-[10px] text-green-500 pr-1"></i>
+                        <span className='truncate max-w-[120px]'>{user}</span>
+                    </div>
                 ))}
             </ul>
         </div>
