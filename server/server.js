69f3c380186b10697968faec6df77017d4000558
Petko Todorov <p.todorov1015@gmail.com>
2024-07-29 18:13:49 +0300

    keep server alive

--- server/server.js
+++ server/server.js
@@ -163,7 +163,7 @@ io.on('connection', (socket) => {
 
     setInterval(() => {
         socket.emit('ping');
-    }, 60001); 
+    }, 20000); 
 
     socket.on('pong', () => {
         console.log(`Pong received from ${socket.id}`);
